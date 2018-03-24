dec03a2192691ec1704e4847bbaf616f02f92002
DESKTOP-EABCC32\anndi <anndreajd@gmail.com>
2018-03-24 11:19:26 +0100

    new edits

---
+++ welcome.php
@@ -0,0 +1,36 @@
+<?php
+// Initialize the session
+session_start();
+
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: login.php");
+  exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="styles.css">
+    <style type="text/css">
+    <img src="logo.jpg" width="380" height="125" />
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Welcome<b><?php echo $_SESSION['username']; ?></b>, lets Woof and Stream!</h1>
+    </div>
+
+    <img src= "http://89.11.192.6:8081/" >
+    <p><a href="logout.php" class="redbtn">Sign Out of Your Account</a></p>
+</body>
+</html>
+
+
+
+
+
+
